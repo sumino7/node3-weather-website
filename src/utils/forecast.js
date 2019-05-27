486c4bf1e3969bd5dcb5bafe82e4653d8821f2d9
sumino7 <hisasumi@gmail.com>
2019-05-27 16:10:04 +0100

    Addition of temperature high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error){
             callback('Unable to find location',undefined)
         } else {
-            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. The day high is ' + body.daily.data[0].temperatureHigh + '. The day low is ' + body.daily.data[0].temperatureLow)
         }
 
     })
